5bf48d84f15aa951502df61716c6f43ece5ce0ac
nallataifeh <nallataifeh@gmail.com>
2022-11-21 07:24:41 +0000

    Code Synced by DhiWise

--- src/service/api.js
+++
@@ -1,7 +0,0 @@
-import { apis } from "service";
-
-const COMMON_URL = `https://thehelpinghandapp.com/user/login/`;
-
-const API_URLS = { POST: `${COMMON_URL}` };
-
-export const post = (payload) => apis.post(API_URLS.POST, payload);
